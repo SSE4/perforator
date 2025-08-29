4e0d5b7e0db6151922a3719bb8afeaf46405ba83
Mikail Bagishov <mikailbag@yandex-team.com>
2025-08-29 19:12:31 +0300

    better

--- perforator/ci/scripts/build.sh
+++ perforator/ci/scripts/build.sh
@@ -4,6 +4,17 @@ set -euxo pipefail
 
 mkdir ~/src
 
+df
+
 (cd ~/src && tar xf ~/code.tgz)
 
-(cd ~/src && ./ya test -T -DCI=github -DCONSISTENT_BUILD=yes -DCONSISTENT_DEBUG=yes ./perforator)
+
+if [[ -z "${CACHE_RW}" ]]; then
+    BAZEL_PUT_ARGS=""
+else
+    BAZEL_PUT_ARGS="--bazel-remote-put --bazel-remote-username=${BAZEL_CACHE_USER} --bazel-remote-password-file=${BAZEL_CACHE_PASSWORD_PATH}"
+fi
+
+(cd ~/src && ./ya test -T -DCI=github -DCONSISTENT_BUILD=yes -DCONSISTENT_DEBUG=yes --bazel-remote-store --bazel-remote-uri=${BAZEL_URI} ${BAZEL_PUT_ARGS}  ./perforator)
+
+df
